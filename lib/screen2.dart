f17e140743c099b5b279160e37ea6fdb41312882
Yuta iwasaki <yanyong0127@gmail.com>
2022-03-10 14:38:13 +0900

    ウェジェットのライフサイクルについて

--- lib/screen2.dart
+++ lib/screen2.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
 // ignore: use_key_in_widget_constructors
-class Screen2 extends StatelessWidget {
+class Screen2 extends StatefulWidget {
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
+  @override
+  //画面が作成される前に最初によばれる
+  void initState() {
+    super.initState();
+    print('initState colled');
+  }
+//画面作成時
   @override
   Widget build(BuildContext context) {
+      print('build colled');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
@@ -25,5 +38,11 @@ class Screen2 extends StatelessWidget {
         ),
       ),
     );
+  }
+  //画面が壊されたとき
+  @override
+  void deactivate() {
+    super.deactivate();
+      print('deactivate colled');
   }
 }
